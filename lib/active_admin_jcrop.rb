7e793ffcaa40bacf3d43fb5c7981c9ddae660afa
fidenz-kelum <kelum@fidenz.com>
2020-12-02 16:37:53 +0530

    temp fix

--- lib/active_admin_jcrop.rb
+++ lib/active_admin_jcrop.rb
@@ -1,9 +1,9 @@
 require "activeadmin"
-require "active_admin_jcrop/dsl"
-require 'active_admin_jcrop/asset_engine'
-require 'active_admin_jcrop/orm'
-require "formtastic/inputs/jcrop_input"
-require "active_admin_jcrop/engine"
-require "active_admin_jcrop/railtie"
-require 'active_admin_jcrop/image_helper'
-require 'active_admin_jcrop/view_helpers'
+#require "active_admin_jcrop/dsl"
+#require 'active_admin_jcrop/asset_engine'
+#require 'active_admin_jcrop/orm'
+#require "formtastic/inputs/jcrop_input"
+#require "active_admin_jcrop/engine"
+#require "active_admin_jcrop/railtie"
+#require 'active_admin_jcrop/image_helper'
+#require 'active_admin_jcrop/view_helpers'
